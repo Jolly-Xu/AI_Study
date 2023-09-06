1b09c896277a13b516b32fa6b9f9cf3109dfcf04
Jolly-Xu <1589112546@qq.com>
2023-09-06 22:03:29 +0800

    第一次提交

--- LSTM.py
+++ LSTM.py
@@ -4,7 +4,7 @@ import numpy as np
 import pickle as pk
 
 # 参数
-train_X = [[]]
+train_X = []
 train_Y = []
 padding_size = 10
 UNK, PAD = '<UNK>', '<PAD>'
@@ -19,10 +19,12 @@ class LSTM(nn.Module):
         self.embedding = nn.Embedding(vocabSize, embeddingDim)
         self.Lstm = nn.LSTM(input_size, hidden_size, layers, bidirectional=True, batch_first=True)
         self.fc = nn.Linear(hidden_size * 2, output_size)
+        self.device = torch.device("cuda")
 
     def forward(self, train_data):
-        h0 = torch.zeros(self.num_layers, train_data.size(1), self.hidden_size).to(train_data.device)
-        c0 = torch.zeros(self.num_layers, train_data.size(1), self.hidden_size).to(train_data.device)
+        h0 = torch.zeros(self.num_layers, self.hidden_size).to(self.device)
+        c0 = torch.zeros(self.num_layers, self.hidden_size).to(self.device)
+        train_data = self.embedding(train_data)
         out, _ = self.lstm(train_data, (h0, c0))
         out = self.fc(out[-1, :, :])
         return out
@@ -31,24 +33,33 @@ class LSTM(nn.Module):
 # 数据预处理
 # 读取本地训练数据
 # 加载语料表构建数据集
-# with open("./data/vocab.pkl", "rb") as fp:
-#     pkl = pk.load(fp)
-#
-# with open("./data/train.txt", encoding="utf-8") as f:
-#     for line in f:
-#         train_arr = line.strip().split("\t")
-#         # 对文字进行分字处理
-#         temp = list(train_arr[0])
-#         if len(temp) >= padding_size:
-#             temp = temp[:padding_size]
-#         else:
-#             temp.extend((padding_size - len(temp)) * PAD)
-#         # 读取每一个字在字典中的value
-#         words_value = []
-#         for word in temp:
-#             words_value.append(pkl.get(word))
-#         train_X.append(words_value)
-#         train_Y.append(train_arr[1])
+with open("./data/vocab.pkl", "rb") as fp:
+    pkl = pk.load(fp)
+
+with open("./data/train.txt", encoding="utf-8") as f:
+    for line in f:
+        train_arr = line.strip().split("\t")
+        # 对文字进行分字处理
+        temp = list(train_arr[0])
+        if len(temp) >= padding_size:
+            temp = temp[:padding_size]
+        else:
+            temp.extend((padding_size - len(temp)) * PAD)
+        # 读取每一个字在字典中的value
+        words_value = []
+        for word in temp:
+            w = pkl.get(word)
+            w = 5000 if w is None else w
+            words_value.append(w)
+        try:
+            train_X.append(torch.tensor(words_value, dtype=torch.float32))
+        except TypeError:
+            print(words_value)
+        train_Y.append(torch.tensor(float(train_arr[1]), dtype=torch.float32))
+
+# 数据转化为tensor
+torch.stack(train_X)
+torch.stack(train_Y)
 
 # 读取词向量
 embedding_predict = torch.tensor \
@@ -58,5 +69,3 @@ embedding_predict = torch.tensor \
 # 词向量大小
 vocab_size = len(embedding_predict)
 embedding_dim = len(embedding_predict[0])
-
-
